189e32ce58b01da6f4e415fbbdbf5403d9c975ff
Nkhil Katekhaye <katekhaye.nikhil@gmail.com>
2020-09-08 01:33:43 +0530

    Added Test from Dao class

---
+++ app/src/main/java/com/nikhil/androidtestingyt/data/local/ShoppingItem.kt
@@ -0,0 +1,15 @@
+package com.nikhil.androidtestingyt.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "shopping_items")
+data class ShoppingItem (
+    var name : String,
+    var amount : Int,
+    var price: Float,
+    var imageUrl : String,
+
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null
+)
