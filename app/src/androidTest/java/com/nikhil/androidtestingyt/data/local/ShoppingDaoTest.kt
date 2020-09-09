ae69f4f088c2b113c392c472e8e88f28f7b9d6fa
Nkhil Katekhaye <katekhaye.nikhil@gmail.com>
2020-09-10 00:25:02 +0530

    Tested viewmodel

--- app/src/androidTest/java/com/nikhil/androidtestingyt/data/local/ShoppingDaoTest.kt
+++ app/src/androidTest/java/com/nikhil/androidtestingyt/data/local/ShoppingDaoTest.kt
@@ -7,6 +7,8 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
 import com.google.common.truth.Truth.assertThat
 import com.nikhil.androidtestingyt.getOrAwaitValue
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.After
@@ -14,26 +16,28 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import javax.inject.Inject
+import javax.inject.Named
 
 @ExperimentalCoroutinesApi
-@RunWith(AndroidJUnit4::class)
 @SmallTest
+@HiltAndroidTest
 class ShoppingDaoTest {
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
 
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    private lateinit var database: ShoppingItemDatabase
+    @Inject
+    @Named("test_db")
+    lateinit var database: ShoppingItemDatabase
     private lateinit var dao : ShoppingDao
 
     @Before
     fun setup(){
-
-        database = Room.inMemoryDatabaseBuilder(
-            ApplicationProvider.getApplicationContext(),
-            ShoppingItemDatabase::class.java
-        ).allowMainThreadQueries().build()
-
+        hiltRule.inject()
         dao = database.shoppingDao()
     }
 
@@ -51,7 +55,6 @@ class ShoppingDaoTest {
         assertThat(allShoppingItem).contains(shoppingItem)
     }
 
-
     @Test
     fun deleteShoppingItem() = runBlockingTest {
 
@@ -62,20 +65,17 @@ class ShoppingDaoTest {
         assertThat(allShoppingItem).doesNotContain(shoppingItem)
     }
 
-
     @Test
     fun observeTotalPriceSum() = runBlockingTest {
 
         val shoppingItem1 = ShoppingItem("name", 2,10f,"url" , id = 1)
         val shoppingItem2 = ShoppingItem("name", 4,5.5f,"url" , id = 2)
         val shoppingItem3 = ShoppingItem("name", 0,100f,"url" , id = 3)
-
         dao.insertShoppingItem(shoppingItem1)
         dao.insertShoppingItem(shoppingItem2)
         dao.insertShoppingItem(shoppingItem3)
 
         var totalPriceSum =  dao.observeTotalPrice().getOrAwaitValue()
-
         assertThat(totalPriceSum).isEqualTo(2 * 10f + 4 * 5.5f)
     }
 }
--- app/src/main/java/com/nikhil/androidtestingyt/ui/ShoppingViewModel.kt
+++ app/src/main/java/com/nikhil/androidtestingyt/ui/ShoppingViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.nikhil.androidtestingyt.data.local.ShoppingItem
 import com.nikhil.androidtestingyt.data.remote.responses.ImageResponse
+import com.nikhil.androidtestingyt.other.Constants
 import com.nikhil.androidtestingyt.other.Event
 import com.nikhil.androidtestingyt.other.Resource
 import com.nikhil.androidtestingyt.repositories.ShoppingRepository
@@ -45,13 +46,79 @@ class ShoppingViewModel @ViewModelInject constructor(
     }
 
 
-    fun insertShoppingItem(name : String , amountString : String , priceString : String ) {
+    fun insertShoppingItem(name: String, amountString: String, priceString: String) {
 
+        if (name.isEmpty() || amountString.isEmpty() || priceString.isEmpty()) {
+            _insertShoppingItemStatus.postValue(
+                Event(
+                    Resource.error(
+                        "The field is not empty",
+                        null
+                    )
+                )
+            )
+            return
+        }
+
+        if (name.length > Constants.MAX_NAME_LENGTH) {
+            _insertShoppingItemStatus.postValue(
+                Event(
+                    Resource.error(
+                        "The name of the item must not exceed ${Constants.MAX_NAME_LENGTH} characters",
+                        null
+                    )
+                )
+            )
+
+            return
+
+        }
+
+        if (priceString.length > Constants.MAX_PRICE_LENGTH) {
+            _insertShoppingItemStatus.postValue(
+                Event(
+                    Resource.error(
+                        "The Price of the item must not exceed ${Constants.MAX_PRICE_LENGTH} characters",
+                        null
+                    )
+                )
+            )
+            return
+        }
+
+        val amount = try {
+            amountString.toInt()
+        } catch (e: Exception) {
+            _insertShoppingItemStatus.postValue(
+                Event(
+                    Resource.error(
+                        "Please enter a valid amount",
+                        null
+                    )
+                )
+            )
+            return
+        }
+
+        val shoppingItem =
+            ShoppingItem(name, amount, priceString.toFloat(), _curImagesUrl.value ?: "")
+        insertShoppingItemIntoDb(shoppingItem)
+        setCurImageUrl("")
+        _insertShoppingItemStatus.postValue(Event(Resource.success(shoppingItem)))
     }
 
 
-    fun searchForImage(imageQuery : String ){
+    fun searchForImage(imageQuery: String) {
 
+        if (imageQuery.isEmpty()) {
+            return
+        }
+
+        _images.value = Event((Resource.loading(null)))
+        viewModelScope.launch {
+            val response = repository.searchForImage(imageQuery)
+            _images.value = Event(response)
+        }
     }
 
 }
